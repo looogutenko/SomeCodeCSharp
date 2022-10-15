77523e0bcbc256452771f1ba715ff1dc0f28bc04
ilooogutenko <96056631+looogutenko@users.noreply.github.com>
2022-10-16 00:26:49 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SomeCode
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(AlphabetPosition("sdfnjksdn"));
+        }
+
+
+
+        //15.10.2022
+        public static string AlphabetPosition(string text) =>
+            string.Join(" ", text
+                .ToLower()
+                .Select(letter => Convert.ToInt32(letter) - 'a' + 1));
+
+    }
+}

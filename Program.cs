245660921c7462905f65336b094ac50b5fd0add0
ilooogutenko <96056631+looogutenko@users.noreply.github.com>
2022-10-16 02:58:10 +0300

    16.10.2022

--- Program.cs
+++ Program.cs
@@ -9,14 +9,42 @@ namespace SomeCode
     {
         public static void Main()
         {
-            Console.WriteLine(AlphabetPosition("sdfnjks dn"));
+            Console.WriteLine(DuplicateCount("aabbcde"));
         }
+
+
 
         //15.10.2022
         public static string AlphabetPosition(string text) =>
             string.Join(" ", text
                 .ToLower()
-                .Where(letter => letter != ' ')
+                .Where(letter => Char.IsLetter(letter))
                 .Select(letter => Convert.ToInt32(letter) - 'a' + 1));
+
+
+
+        //16.10.2022
+        public static int DuplicateCount(string str)
+        {
+            List<int> counts = new(str.ToLower().Distinct().Count());
+
+            int count;
+            foreach (char UniqueCh in str.ToLower().Distinct())
+            {
+                count = 0;
+                foreach (char Ch in str)
+                {
+                    if (UniqueCh == Ch)
+                    {
+                        count++;
+                    }
+                }
+                counts.Add(count);
+            }
+
+            return counts
+                .Where(count => count > 1)
+                .Count();
+        }
     }
 }
